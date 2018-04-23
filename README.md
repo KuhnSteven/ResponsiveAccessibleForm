b1f42199b3a98b4ea094d66dd24d374ec7d55a25
Steve Kuhn <SRKuhn@student.fullsail.edu>
2018-04-22 22:29:04 -0400

    updates to index, css, js amd readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ResponsiveAccessibleForm
 This project is intended to simulate a shopping cart checkout form. The first intent is to have a fully responsive form, that fits both mobile and desktop (mobile 1st). The second intent, is that any obvious accessibility flaws should be met, to make the form/website as user-friendly as possible.
+
+##Live Demo
+https://kuhnsteven.github.io/ResponsiveAccessibleForm/
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Variables
 const form = document.querySelector("#form");
+const feedback = document.getElementById("#formFeedback");
 let size = "S";
 let color = 1;
 let name = "";
@@ -11,29 +12,34 @@ let state = "";
 let zip = 0;
 let country = "";
 
+let content = '';
+
 form.addEventListener("submit", e => {
     e.preventDefault();
-
     return formValidation();
 })
 
 function formValidation() {
-    size = document.querySelector("#size");
-    color = document.querySelector("#color");
+    size = document.getElementById("size");
+    color = document.getElementById("color");
 
-    name = document.querySelector("#name").nodeValue;
-    email = document.querySelector("#email");
+    name = document.getElementById("name");
+    email = document.getElementById("email");
 
-    address = document.querySelector("#sAddress");
-    address2 = document.querySelector("#sAddress2");
-    city = document.querySelector("#city");
-    state = document.querySelector("#state");
-    zip = document.querySelector("#zip");
-    country = document.querySelector("#country");
+    address = document.getElementById("sAddress");
+    address2 = document.getElementById("sAddress2");
+    city = document.getElementById("city");
+    state = document.getElementById("state");
+    zip = document.getElementById("zip");
+    country = document.getElementById("country");
 
     if (name != "" && email !="" && address !="" && city !="" && state != "" && zip != 0 && country != "") {
         // Valid form submission
-        
+        content +=
+            `ORDER DETAILS<br>Product Size: ${size}<br>Product Color: ${color}<br><br>Customer Name: ${name}<br>Customer Email: ${email}<br>Shipping Address 1: ${sAddress}<br>Shipping Address 2: ${sAddress2}<br>City: ${city}<br>State: ${state}<br>Zip: ${zip}<br>Country: ${country}`;
+
+        // Populate the DOM
+        feedback.innerHTML(content);
     }
 
     // Invalid form alerts
